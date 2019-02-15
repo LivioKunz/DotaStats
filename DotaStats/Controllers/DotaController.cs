73f93f8a1715732fd6ea2b1be05c725d5fa0ecfa
Livio Kunz <livio_kunz@hotmail.com>
2019-02-15 17:04:34 +0100

    Heldenwinrate lesen

--- DotaStats/Controllers/DotaController.cs
+++ DotaStats/Controllers/DotaController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +10,11 @@ using Newtonsoft.Json.Linq;
 namespace DotaStats.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Dota")]
     public class DotaController : Controller
     {
         // GET
         [HttpGet]
+        [Route("api/Hero")]
         public async Task<IEnumerable<Hero>> GetHeroes()
         {
             var api = new OpenDotaApi();
@@ -22,6 +23,34 @@ namespace DotaStats.Controllers
 
             var heroes = JsonConvert.DeserializeObject<List<Hero>>(heroesJson);
             return heroes;
+
+        }
+
+        [HttpGet]
+        [Route("api/Hero/{name}")]
+        public async Task<IEnumerable<Hero>> GetHeroes(string name)
+        {
+            var api = new OpenDotaApi();
+
+            var heroesJson = api.GetHeroes();
+
+            var heroes = JsonConvert.DeserializeObject<List<Hero>>(heroesJson);
+            var searchedHero = heroes.SingleOrDefault(x => x.localized_name == name);
+
+            var winRateHeroes =
+                JsonConvert.DeserializeObject<List<WinRates>>(
+                    $"https://api.opendota.com/api/heroes/{searchedHero.id}/matchups");
+
+            var bestWinrates = winRateHeroes.Where(x => x.games_played > 10).OrderBy(x => x.wins).Take(10);
+
+            var worstWinrates = winRateHeroes.Where(x => x.games_played > 10).OrderByDescending(x => x.wins).Take(10);
+
+            var returnList = new List<Hero>();
+            returnList.Add(searchedHero);
+            //todo listen zusammenführen und helden zurückgeben
+            //var bestWinratesHeroes = 
+
+            return returnList;
 
         }
     }
@@ -36,5 +65,15 @@ namespace DotaStats.Controllers
         public string attack_type { get; set; }
         public List<string> roles { get; set; }
         public int legs { get; set; }
+
+        public string winRateFavor { get; set; }
+    }
+
+    [Serializable]
+    public class WinRates
+    {
+        public int hero_id { get; set; }
+        public int games_played { get; set; }
+        public int wins { get; set; }
     }
 }
